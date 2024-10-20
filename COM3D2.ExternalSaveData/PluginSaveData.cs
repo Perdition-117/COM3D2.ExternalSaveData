53ba8174e0322aa0931c70a4f9aedd33d129af93
Perdition <87424475+Perdition-117@users.noreply.github.com>
2024-10-20 15:48:01 +0200

    Add convenience client API

---
+++ COM3D2.ExternalSaveData/PluginSaveData.cs
@@ -0,0 +1,33 @@
+namespace CM3D2.ExternalSaveData.Managed;
+
+public class PluginSaveData {
+	private readonly string _pluginName;
+
+	public PluginSaveData(string pluginName) {
+		_pluginName = pluginName;
+	}
+
+	public MaidSaveData GetMaidData(Maid maid) {
+		return new MaidSaveData(_pluginName, maid);
+	}
+
+	public class MaidSaveData {
+		private readonly string _pluginName;
+		private readonly Maid _maid;
+
+		public MaidSaveData(string pluginName, Maid maid) {
+			_pluginName = pluginName;
+			_maid = maid;
+		}
+
+		public string GetString(string propName, string defaultValue) => ExSaveData.Get(_maid, _pluginName, propName, defaultValue);
+		public bool GetBoolean(string propName, bool defaultValue) => ExSaveData.GetBool(_maid, _pluginName, propName, defaultValue);
+		public int GetInteger(string propName, int defaultValue) => ExSaveData.GetInt(_maid, _pluginName, propName, defaultValue);
+		public float GetFloat(string propName, float defaultValue) => ExSaveData.GetFloat(_maid, _pluginName, propName, defaultValue);
+
+		public void SetString(string propName, string value) => ExSaveData.Set(_maid, _pluginName, propName, value);
+		public void SetBoolean(string propName, bool value) => ExSaveData.SetBool(_maid, _pluginName, propName, value);
+		public void SetInteger(string propName, int value) => ExSaveData.SetInt(_maid, _pluginName, propName, value);
+		public void SetFloat(string propName, float value) => ExSaveData.SetFloat(_maid, _pluginName, propName, value);
+	}
+}
